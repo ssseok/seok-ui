621371e8b751c8d34e70503bfc048048c2e74730
Hyun Seok Park <luckseok1@gmail.com>
2024-08-14 17:46:55 +0900

    :sparkles: Implement IconButton Component

---
+++ src/components/IconButton.tsx
@@ -0,0 +1,20 @@
+interface IIconButtonProps {
+    iconPath: string;
+    alt: string;
+    onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function IconButton({
+    iconPath,
+    onClick,
+    alt,
+}: IIconButtonProps) {
+    return (
+        <button onClick={onClick}>
+            <img
+                alt={alt}
+                src={iconPath}
+            />
+        </button>
+    );
+}
