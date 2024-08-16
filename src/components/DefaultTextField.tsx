380355d6700c42666c27778ce873004c4c5d6ec9
Hyun Seok Park <luckseok1@gmail.com>
2024-08-16 16:05:28 +0900

    ✨ Implement TextField Component

---
+++ src/components/DefaultTextField.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import ErrorMessge from './ErrorMessge';
+import IconButton from './IconButton';
+
+interface IDefaultTextField {
+    errorMessge: string;
+    iconPath: string;
+    onIconClick: React.MouseEventHandler<HTMLButtonElement>;
+    onChange: React.ChangeEventHandler<HTMLInputElement>;
+    IconAlt: string;
+    value: string;
+    placeholder: string;
+    isError: boolean;
+}
+
+export default function DefaultTextField({
+    IconAlt,
+    errorMessge,
+    iconPath,
+    onChange,
+    onIconClick,
+    placeholder,
+    value,
+    isError,
+}: IDefaultTextField) {
+    const [isFocused, setIsFocused] = useState<boolean>(false);
+    const borderColor = isFocused ? "border-secondary" :  !value? 'border-tree300' : 'border-primary';
+    return (
+        <div
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+            className={`
+              text-primary
+              border-b
+              ${borderColor}
+              `}
+        >
+            <input
+                type="text"
+                onChange={onChange}
+                placeholder={placeholder}
+                value={value}
+            />
+            {!!value && (
+                <IconButton
+                    onClick={onIconClick}
+                    alt={IconAlt}
+                    iconPath={iconPath}
+                />
+            )}
+
+            {isError && <ErrorMessge>{errorMessge}</ErrorMessge>}
+        </div>
+    );
+}
